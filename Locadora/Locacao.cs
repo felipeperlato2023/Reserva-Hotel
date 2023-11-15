629cb8fe477b79ea012d3629fd608b31ac7ea474
Felipe Perlato <fe.perlato@gmail.com>
2023-11-14 21:54:30 -0300

    primeiro commit

---
+++ Locadora/Locacao.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Locadora
+{
+    internal class Locacao
+    {
+        public string Modelocarro { get; set; }
+
+        public DateTime Datasaida { get; set; }
+
+        public DateTime Dataretorno { get; set; }
+
+        public double Valorhora { get; set; }
+
+        public double Valordiario { get; set; }
+
+
+        public Locacao()
+        {
+
+        }
+
+        public Locacao(string modelocarro, DateTime datasaida, DateTime dataretorno, double valorhora, double valordiario)
+        {
+            Modelocarro = modelocarro;
+            Datasaida = datasaida;
+            Dataretorno = dataretorno;
+            Valorhora = valorhora;
+            Valordiario = valordiario;
+        }
+
+        public int Locar()
+        {
+            TimeSpan duracao = Dataretorno.Subtract(Datasaida);
+
+
+
+            return (int)duracao.Hours;
+
+        }
+
+
+        public override string ToString()
+        {
+            return " modelo:"
+                + Modelocarro
+                 + " dataalugel:"
+                  + Datasaida.ToString("dd/MM/yyyy ")
+                   + " dataretorno:"
+                    + Dataretorno.ToString("dd/MM/yyyy ")
+                     + Locar()
+                      + " horas";
+
+        }
+    }
+}
---
+++ Locadora/Program.cs
@@ -0,0 +1,27 @@
+﻿using Locadora;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Modelo do carro");
+        string modelo = Console.ReadLine();
+
+        Console.WriteLine("data de saida");
+        DateTime datasaida = DateTime.Parse(Console.ReadLine());
+
+        Console.WriteLine("data retorno");
+        DateTime dataretorno=DateTime.Parse(Console.ReadLine());
+
+        Console.WriteLine("Valor por hora: ");
+        double valorhora = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("valor diario");
+        double valordiario = double.Parse(Console.ReadLine());
+
+        Locacao loc = new Locacao(modelo,  datasaida, dataretorno, valorhora, valordiario);
+
+      
+        Console.WriteLine(loc);
+    }
+}
